137946df4662e16b4b9d52fffc49a2fd34ced463
srijonsarkar1 <srijon.sarkar@oracle.com>
2022-01-26 20:06:21 +0530

    first commit

---
+++ app_flask.py
@@ -0,0 +1,40 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('classifier.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    #return render_template('index.html')
+    return {'message' : 'Hello Oracle'}
+
+@app.route('/predict', methods=['POST'])
+def predict_banknote(data:BankNote):
+    """
+        for rendering result on HTML GUI
+    """
+
+    data = data.dict()
+    print(data)
+    variance = data['variance']
+    skewness = data['skewness']
+    curtosis = data['curtosis']
+    entropy = data['entropy']
+
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
+
+    if prediction[0]>0.5:
+        prediction = "Fake Note"
+    else:
+        prediction = "Bank Note"
+
+    return render_template('index.html', prediction_text = f'Specified note is a ${prediction}')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    #main()
